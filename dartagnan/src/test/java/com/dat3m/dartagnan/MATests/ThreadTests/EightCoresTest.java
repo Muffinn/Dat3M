1754e3fa521ea28a87d1c8e5b78e548cf67f96bc
fth <f.thieme@tu-bs.de>
2022-12-13 03:44:06 +0100

    added EightCoresTest added linearRefinementTest

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/MATests/ThreadTests/EightCoresTest.java
@@ -0,0 +1,98 @@
+package com.dat3m.dartagnan.MATests.ThreadTests;
+
+import com.dat3m.dartagnan.c.AbstractCTest;
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.rules.CSVLogger;
+import com.dat3m.dartagnan.utils.rules.Provider;
+import com.dat3m.dartagnan.verification.model.ExecutionModel;
+import com.dat3m.dartagnan.verification.solving.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.java_smt.SolverContextFactory;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import static com.dat3m.dartagnan.configuration.Arch.*;
+import static com.dat3m.dartagnan.utils.ResourceHelper.TEST_RESOURCE_PATH;
+import static com.dat3m.dartagnan.utils.Result.UNKNOWN;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class EightCoresTest extends AbstractCTest {
+
+    private String reportFileName;
+    private int[] nAndM;
+
+    public EightCoresTest(String name, Arch target, Result expected, String reportFileName, int[] nAndM) {
+        super(name, target, expected);
+        this.reportFileName = reportFileName;
+        this.nAndM = nAndM;
+    }
+
+    @Override
+    protected Provider<String> getProgramPathProvider() {
+        return Provider.fromSupplier(() -> TEST_RESOURCE_PATH + "lfds/" + name + ".bpl");
+    }
+
+    @Override
+    protected long getTimeout() {
+        return 600000;
+    }
+
+    protected Provider<Integer> getBoundProvider() {
+        return Provider.fromSupplier(() -> 2);
+    }
+
+	@Parameterized.Parameters(name = "{index}: {0}, target={1}")
+    public static Iterable<Object[]> data() throws IOException {
+		Object[] jitDelete = new Object[]{"treiber-3", C11, UNKNOWN, "JIT_Delete", new int[]{1, 1}};
+        LinkedList<Object[]> objectList = new LinkedList<Object[]>();
+        objectList.add(jitDelete);
+        String testName = "EightCoresTest";
+
+        String[] programNames = {"dglm-3", "ms-3", "treiber-3"};
+        Arch[] wmms = {TSO, ARM8, C11};
+
+
+        int[][] nAndMArray = {{1,1}, {2,1}, {2,2}, {3,2}, {2,3}};
+        for(int[] nAndM : nAndMArray){
+            for (Arch wmm:wmms) {
+                for (String programName : programNames) {
+                    for (int j = 0; j < 2; j++) {
+                        Object[] objects = new Object[]{programName, wmm, UNKNOWN, testName, nAndM};
+                        objectList.add(objects);
+                    }
+                }
+            }
+        }
+        return objectList;
+    }
+
+
+    @Test
+    @CSVLogger.FileName("csv/parallelRefinement")
+    public void testParallelRefinement() throws Exception {
+        ParallelSolverConfiguration parallelConfig = ParallelSolverConfigurationFactory.scoredEventConfig();
+        parallelConfig.setSplittingStyle(ParallelSolverConfiguration.SplittingStyle.LINEAR_AND_BINARY_SPLITTING_STYLE);
+        parallelConfig.setSplittingIntN(nAndM[0]);
+        parallelConfig.setSplittingIntM(nAndM[1]);
+        parallelConfig.setMaxNumberOfConcurrentThreads(8);
+
+
+        parallelConfig.initializeFileReport(reportFileName, target.toString(), name, "PR");
+
+        ParallelRefinementSolver s = ParallelRefinementSolver.run(contextProvider.get(), proverProvider.get(),
+                taskProvider.get(), SolverContextFactory.Solvers.Z3, Configuration.defaultConfiguration(),
+                shutdownManagerProvider.get(), parallelConfig);
+        assertEquals(expected, s.getResult());
+    }
+
+
+
+
+
+}
